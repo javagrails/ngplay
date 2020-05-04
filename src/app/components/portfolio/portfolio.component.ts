3309c8867965758ca85e293f22a87d50b7123ac9
gh_javagrails <salman1m22@gmail.com>
2020-05-04 17:08:49 +0600

    05. Animating Our Portfolio List

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,3 +1,4 @@
+import {animate, animateChild, query, stagger, style, transition, trigger} from '@angular/animations';
 import {Component, OnInit} from '@angular/core';
 import {Portfolio} from '../../services/portfolio.model';
 import {PortfolioService} from '../../services/portfolio.service';
@@ -5,7 +6,21 @@ import {PortfolioService} from '../../services/portfolio.service';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(60, animateChild()))
+      ])
+    ]),
+    trigger('items', [
+      transition(':enter', [
+        style({transform: 'scale(0)', opacity:0}),
+        animate('0.350s cubic-bezier(0.8, -0.6, 0.2, 1.5)'),
+        style({transform: 'scale(1)', opacity:1})
+      ])
+    ])
+  ]
 })
 export class PortfolioComponent implements OnInit {
 
